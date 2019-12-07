7c7893076b4f533b3ee9136182d7d9adef3fda5d
rchen102 <rchen102@binghamton.edu>
2019-12-06 23:51:41 -0500

    cs435: assign2

---
+++ assign2/README.md
@@ -0,0 +1,17 @@
+# CS435: Assignment 2
+
+## Basic Information
+Name: Renze Chen  
+B-Number: B00767193  
+
+## Question1
+
+## Question2
+
+## Question3
+
+## Question4
+
+## Question5
+
+
---
+++ assign2/data_preprocess.m
@@ -0,0 +1,15 @@
+clear, clc;
+load('raw.mat');
+
+%% Fill missing value 
+mean_v = nanmean(raw);
+for i = 1 : length(mean_v)
+    idx = isnan(raw(:,i));
+    raw(idx,i) = mean_v(i);
+end
+    
+%% Normalize data
+raw = raw';
+raw = mapminmax(raw, 0, 1);
+raw = raw';
+data = raw;
