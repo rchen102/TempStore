95e46f8b1fb6a7e40a901480cc2da23ac6cdaa8d
rchen102 <rchen102@binghamton.edu>
2020-08-28 02:22:37 -0400

    empty

--- branch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash +vx
-LIB_PATH=./lib/protobuf-java-3.7.0.jar
-
-#name port interval 
-java -classpath bin:$LIB_PATH assig3.branch.BranchDriver $1 $2 $3
--- controller.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash +vx
-LIB_PATH=./lib/protobuf-java-3.7.0.jar
-
-#totalMoney fileName
-java -classpath bin:$LIB_PATH assig3.controller.ControllerDriver $1 $2
